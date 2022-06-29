ac4c8fa28e41b7649d77d89a353cd4546d01da71
Felix <felixazeller@gmail.com>
2022-06-29 12:23:17 -0400

    Javascript testing of graph, need more testing eventually

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,5 +1,19 @@
 import * as wasm from 'webnav_analysis'
-chrome.history.search({ text: "", maxResults: 100000, startTime: 987532627000 }).then(r => wasm.get(r))
+chrome.history.search({ text: "", maxResults: 100000, startTime: 987532627000 }).then(r => {
+  console.log(r)
+
+  wasm.test()
+  wasm.test()
+
+  let searchProcess = new wasm.SearchProcess(r)
+  console.log(searchProcess.get_graph())
+
+  console.log(searchProcess.get_edges("Courses", "https://barringtonschools.instructure.com/courses"))
+
+  for (let i = 0; i < 10; i++) console.log(searchProcess.get_edges("Courses", "https://barringtonschools.instructure.com/courses"))
+})
+
+
 
 
 
